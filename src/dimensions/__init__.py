1420527de299fc6a65c15e6d41f553ee3e4db3bc
Ivanilson Sousa de Oliveira <ivanilsonsousa@sobral.ce.gov.br>
2024-09-30 10:12:49 -0300

    removido contexto geral, e ocultado por enquanto reconhecimento de padrões

--- src/dimensions/__init__.py
+++ src/dimensions/__init__.py
@@ -3,5 +3,5 @@ from .reconhecimento_padroes import config as reconhecimento_padroes_config
 
 dimensions = {
   "2": decomposicao_config,
-  "3": reconhecimento_padroes_config,
+  # "3": reconhecimento_padroes_config,
 }
